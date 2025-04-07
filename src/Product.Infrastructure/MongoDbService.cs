b1b1d6e13b510d40ee4df17faeb0f739c806bfb2
Bartosz Panic <bartosz.panic@gmail.com>
2025-04-07 20:08:08 +0200

    Add Product.Infrastructure project and MongoDB service implementation

---
+++ src/Product.Infrastructure/MongoDbService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Product.Infrastructure;
+
+public class MongoDbService
+{
+    private readonly IConfiguration _configuration;
+    private readonly IMongoDatabase _database;
+
+    public MongoDbService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        var client = new MongoClient(_configuration.GetConnectionString("MongoDb"));
+        _database = client.GetDatabase(_configuration["DatabaseName"]);
+    }
+
+    public IMongoDatabase? Database => _database;
+}
--- src/Shared/Product.cs
+++ src/Shared/Product.cs
@@ -1,6 +1,16 @@
-﻿namespace Shared;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Shared;
 
 public record Product
 {
+    [BsonId]
+    [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+    public Guid Id { get; init; } = Guid.NewGuid();
 
+    [BsonElement("product_name")]
+    public string ProductName { get; set; } = string.Empty;
+    [BsonElement("product_price")]
+    public decimal Price { get; set; }
 }
